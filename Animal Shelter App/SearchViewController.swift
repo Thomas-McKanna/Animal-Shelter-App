c2c1ed914b7326b570a6ce809c33185573945ce6
Thomas McKanna <thomas@Thomass-Air.attlocal.net>
2016-11-04 14:30:46 -0500

    connected UI to code

--- Animal Shelter App/SearchViewController.swift
+++ Animal Shelter App/SearchViewController.swift
@@ -11,6 +11,18 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    // Properties
+    @IBOutlet weak var dogCatEitherLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet weak var maleFemaleEitherLabel: UILabel!
+    @IBOutlet weak var maxAgeLabel: UILabel!
+    @IBOutlet weak var specialNeedsLabel: UILabel!
+    @IBOutlet weak var DogCatEitherSgmtCtrl: UISegmentedControl!
+    @IBOutlet weak var breedTextField: UITextField!
+    @IBOutlet weak var maleFemaleEitherSgmtCtrl: UISegmentedControl!
+    @IBOutlet weak var specialNeedsSwitch: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +31,12 @@ class SearchViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Actions
+    @IBAction func searchButton(_ sender: UIButton) {
+        // encapsulate the user's choice into an object
+        // send that object to the Search Results view, where its information will be used to query the database of available pets
     }
     
 }
