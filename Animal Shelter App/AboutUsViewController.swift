bcedb4c6b925d2eec3df3820962e03263fad5817
Thomas McKanna <thomas@Thomass-MacBook-Air.local>
2016-11-07 19:30:19 -0600

    added view controller for About Us

---
+++ Animal Shelter App/AboutUsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AboutUsViewController.swift
+//  Animal Shelter App
+//
+//  Created by Thomas McKanna on 11/7/16.
+//  Copyright © 2016 ISYS 220. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    // Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
