51c61dd4bb2ba0ff185a2c78b25bd34ae07673f9
Bradley Higdon <bradhigdon@Bradleys-MacBook-Pro.local>
2016-11-07 19:55:22 -0600

    Edit from Brad

--- Animal Shelter App/Constants.swift
+++ Animal Shelter App/Constants.swift
@@ -7,10 +7,11 @@
 //
 
 // TODO: other color? do we need to make a separate list for cats and dogs?
+// Color notes: For cats, there are eight main colors: red (sometimes known as orange), black, chocolate (dark brown), cinnamon (light brown), cream (also referred to as buff or tan), blue (grey), lilac (lavender), and fawn.
 let COLORS = ["Any Color", "Black", "Brown", "White", "Golden", "Mixed"]
 
 // TODO: make complete list (http://cattime.com/cat-breeds)
-let CAT_BREEDS = ["Any Breed", "Abyssinian", "American Bobtail", "American Curl", "American Shorthair", "American Wirehair", "Balinese", "Bengal Cats", "Birman", "Bombay", "British Shorthair", "Burmese", "Burmilla", "Chartreux"]
+let CAT_BREEDS = ["Any Breed", "Abyssinian", "American Bobtail", "American Curl", "American Shorthair", "American Wirehair", "Balinese", "Bengal Cats", "Birman", "Bombay", "British Shorthair", "Burmese", "Burmilla", "Chartreux", "Chinese Li Hua", "Colorpoint Shorthair", "Cornish Rex", "Cymric", "Devon Rex", "Egyptian Mau", "European Burmese", "Exotic", "Havana Brown", "Himalayan", "Japanese Bobtail", "Javanese", "Korat", "LaPerm", "Maine Coon", "Manx", "Nebelung", "Norwegian Forest", "Ocicat", "Oriental", "Persian", "Ragamuffin", "Ragdoll Cats", "Russian Blue", "Savannah", "Scottish Fold", "Selkrirk Rex", "Siamese Cat", "Siberian", "Singapura", "Snowshoe", "Somali", "Sphynx", "Tonkinese", "Turkish Angora", "Turkish Van"]
 
 // TODO: make complete list (http://www.akc.org/news/the-most-popular-dog-breeds-in-america/)
 let DOG_BREEDS = ["Any Breed", "Retriever (Labrador)", "German Sheperd", "Retriever (Golden)", "Bulldog", "Beagle", "French Bulldog", "Yorkshire Terrier"]
