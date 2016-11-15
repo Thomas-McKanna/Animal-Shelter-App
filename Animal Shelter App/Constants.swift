eba127f2c26166975ba72e6d9f139790015d1bd4
Bradley Higdon <bradhigdon@Bradleys-MacBook-Pro.local>
2016-11-14 19:27:47 -0600

    Updated constants file

--- Animal Shelter App/Constants.swift
+++ Animal Shelter App/Constants.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 // TODO: make complete list (http://www.adoptapet.com/adoption_rescue/74766.html)
-let CAT_COLORS = ["Any Color", "Black", "White"]
+let CAT_COLORS = [“Black”, “Black & White or Tuxedo”, “Brown or Chocolate”, “Brown Tabby”, “Calico or Dilute Calico”, “Cream or Ivory”, “Gray, Blue, or Silver Tabby”, “Gray or Blue”, “Orange or Red”, “Orange or Red Tabby”, “Spotted Tabby/Leopard Spotted”, “Tabby”, “Tan or Fawn”, “Tan or Fawn Tabby”, “Tiger Stripped”, “Tortoiseshell”, “White”]
 
 // TODO: make complete list (http://www.adoptapet.com/adoption_rescue/74766.html)
-let DOG_COLORS = ["Any Color", "Black", "White"]
+let DOG_COLORS = [“Black”, “Brindle”, “Brown/Chocolate”, “Gray/Blue/Silver/Salt & Pepper”, “Merle”, “Red/Golden/Orange/Chestnut”, “Silver & Tan”, “Tan/Yellow/Fawn”, “Tricolor”, “White”]
 
 let CAT_AGES = ["Any Age", "Young", "Adult", "Senior", "Kitten"]
 
